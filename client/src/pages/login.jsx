d8423a60ff4c27c68580dfa9a3012c17c0b834f5
Kelly Castillo <kellycastillo123@gmail.com>
2023-10-21 14:59:04 -0500

    small adjustments made to the display

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -2,23 +2,23 @@
 import { Link } from 'react-router-dom'
 import './login.scss'
 
-
 const login = () => {
     return (
         <div className='center'>
             <div className='login'>
                 <div className='left'>
-                    <h1>Ready to trade something old for something new</h1>
+                    <h1>Ready to trade something old, 
+                        for something new!</h1>
+                   
                     <form>
                         <input className="login-input" type="text" placeholder="Username"></input>
                         <input className="login-input" type="password" placeholder="Password"></input>
+                    </form>
                         <span>
                         </span>
                         <button>Login</button>
-                    </form>
-                    <p>Want to create an account</p>
-                    
-                    <span>Sign up here </span>
+                    <p>Want to create an account. Sign up here! </p>
+                   
                     <Link to={"/register"}>
                         <button>Create Your Account</button>
                     </Link>
--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -1,3 +1,4 @@
+
 import { Link } from 'react-router-dom';
 import './register.scss'
 
@@ -5,20 +6,27 @@ const register = () => {
     return (
         <div className='mid'>
             <div className='register'>
-                <h1>Welcome to Things</h1>
-                <span>Ready to Trade, just type in your email, create a username and password</span>
-                <form>
+                <div className='center'>
+                    <h1>Welcome to Things</h1>
+                    
+                    <span>Ready to Trade, just type in your email, create a username and password</span>
+                    
+                    <form>
                     <input className="login-input" type="text" placeholder="Username"></input>
                     <input className="login-input" type="text" placeholder="Email"></input>
                     <input className="login-input" type="password" placeholder="Password"></input>
                     <input className="login-input" type="text" placeholder="Name"></input>
+                    </form>
+
                     <button>Register</button>
-                </form>
-                <span>Do you already have an account? Login here</span>
-                <Link to={"/login"}>
+                    <span>Do you already have an account? Login here</span>
+                    
+                    <Link to={"/login"}>
                     <button>Login</button>
-                </Link>
-            </div>
+                    </Link>
+
+                 </div>
+             </div>
         </div>
     )
 }
