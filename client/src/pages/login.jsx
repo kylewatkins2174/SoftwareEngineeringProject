d2703636d456c0aa9381a18f3ec02ddd34215613
Kelly Castillo <kellycastillo123@gmail.com>
2023-10-21 16:47:38 -0500

    trying to fix merge error

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -8,11 +8,11 @@ const login = () => {
                 <div className='left'>
                     <h1>Ready to trade something old,
                         for something new!</h1>
-                    <form>
+                        <form>
                         <input className="login-input" type="text" placeholder="Username"></input>
                         <input className="login-input" type="password" placeholder="Password"></input>
-                    </form>
-                    <span>
+                        </form>
+                     <span>
                     </span>
                     <button>Login</button>
                     <p>Want to create an account. Sign up here! </p>
