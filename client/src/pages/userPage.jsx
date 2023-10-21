ae845879cf396d9d0c87fb8a008fc4d8bd208a59
Travis <travisiwillis515@gmail.com>
2023-10-21 14:51:45 -0500

    UserPage added

---
+++ client/src/pages/userPage.jsx
@@ -0,0 +1,23 @@
+import './userPage.scss'
+
+const User = () => {
+    /*function saveClick() {
+        alert('Your information has been saved!');
+    }*/
+    
+    return(
+        
+        <div className='body-container'>
+            <div className='info-container'>
+                <input type="file" className='avatar' id="uploader"></input>
+                <p className="userName">myUsername</p>
+                <p className="email">myEmail@shsu.edu</p>
+                <input className="lastName" type="text" placeholder="First Name"></input>
+                <input className="firstName" type="text" placeholder="First Name"></input> 
+                <button id="saveButtonScript" className="saveButton">Save</button>
+            </div>
+        </div>
+        
+    )
+    
+}
