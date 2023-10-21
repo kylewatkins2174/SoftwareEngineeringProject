efc770a8d438b984ff5209b6d4ea022675603b59
Kelly Castillo <kellycastillo123@gmail.com>
2023-10-21 16:37:59 -0500

    trying to fix merge issue

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -6,22 +6,21 @@ const login = () => {
         <div className='center'>
             <div className='login'>
                 <div className='left'>
-                    <h1>Ready to trade something old, 
+                    <h1>Ready to trade something old,
                         for something new!</h1>
-                   
                     <form>
                         <input className="login-input" type="text" placeholder="Username"></input>
                         <input className="login-input" type="password" placeholder="Password"></input>
                     </form>
-                        <span>
-                        </span>
-                        <button>Login</button>
+                    <span>
+                    </span>
+                    <button>Login</button>
                     <p>Want to create an account. Sign up here! </p>
-                   
+
                     <Link to={"/register"}>
                         <button>Create Your Account</button>
                     </Link>
-                     
+
                 </div>
 
             </div>
