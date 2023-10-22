8547d39355a0508a59f7c3fa9b949e757cbd2ab0
Travis <travisiwillis515@gmail.com>
2023-10-22 15:50:37 -0500

    title animation and placeholder homepage style

--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -1,7 +1,17 @@
+import { Link } from 'react-router-dom';
+import './homePage.scss'
+
 const homePage = () =>{
     return(
-        <div>
-            <p>this is the homepage</p>
+        <div className='body-container'>
+            <div className='menu-container'>
+                <Link className="things-titleHome" to="/home">Things</Link>
+                <div className="menuItemRow">
+                    <span className="menuItem">Inventory</span>
+                    <span className="menuItem">Marketplace</span>
+                    <Link className="menuItem" to="/user">Account</Link>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/pages/userPage.jsx
+++ client/src/pages/userPage.jsx
@@ -1,18 +1,24 @@
+import { Link } from 'react-router-dom';
 import './userPage.scss'
+import './register.jsx';
 
-const User = () => {
-    
+function User () {
+   
     return(  
 
         <div className='body-container'>
-            <input type="file" className='avatar'></input>
+            <Link className="things-title" to="/home">Things</Link>
+            <input type="file" id="submitAvatar" accept="image/jpg, image/png" className='avatar'></input>
+            <div id="avatarDisplay"></div>
             <div className='info-container'>
                 <h1>Your Account</h1>
-                <input className="userName" placeholder="Username"></input>
-                <input className="email" placeholder="email"></input>
-                <input className="lastName" type="text" placeholder="First Name"></input>
-                <input className="firstName" type="text" placeholder="Last Name"></input><br/> 
-                <button className="infoButton">Update</button>
+                <form>
+                <input className="userName" placeholder="Username"></input><br/>
+                <input className="email" placeholder="email"></input><br/>
+                <input className="lastName" type="text" placeholder="First Name"></input><br/>
+                <input className="firstName" type="text" placeholder="Last Name"></input><br/><br/>
+                <button className="infoButton" type="submit">Update</button>
+                </form>
             </div>
         </div>
         
