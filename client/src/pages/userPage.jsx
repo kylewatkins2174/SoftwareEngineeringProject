cf1bca6860e4ce6f731c8625afeb797b65a243e0
Travis <travisiwillis515@gmail.com>
2023-10-21 22:32:15 -0500

    Style Change

--- client/src/pages/userPage.jsx
+++ client/src/pages/userPage.jsx
@@ -5,9 +5,9 @@ const User = () => {
     return(  
 
         <div className='body-container'>
+            <input type="file" className='avatar'></input>
             <div className='info-container'>
                 <h1>Your Account</h1>
-                <input type="file" className='avatar' id="uploader"></input>
                 <input className="userName" placeholder="Username"></input>
                 <input className="email" placeholder="email"></input>
                 <input className="lastName" type="text" placeholder="First Name"></input>
