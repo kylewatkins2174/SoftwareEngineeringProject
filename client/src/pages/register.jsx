210e97dab357a03f7a5054c45d0e6e7953d17355
Jonathan Watkins <kylewatkins217424@outlook.com>
2023-10-22 19:33:16 -0500

    finished wiring login/register page

--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -1,13 +1,40 @@
-import { Link } from 'react-router-dom'
 import "./register.scss"
+import { useState } from "react"
+import { Link, useNavigate } from 'react-router-dom'
+import requestServer from "../axios"
+
+
 const Register = () => {
+
+    const navigate = useNavigate()
+
     const [inputs, setInputs] = useState({
         "firstname" : "",
         "lastname" : "",
         "email" : "",
         "username" : "",
-        
+        "password" : ""
     })
+
+    const [err, setErr] = useState(null)
+
+    const handleChange = (e) => {
+        setInputs((prev) => {
+            return{...prev, [e.target.name] : e.target.value}
+        })
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(JSON.stringify(inputs))
+
+        requestServer.post("/auth/register", inputs).then(response => {
+            console.log(response)
+            navigate("/login")
+        }).catch(err => {
+            setErr(err)
+        })
+    }
 
 
     return (
@@ -19,14 +46,17 @@ const Register = () => {
                     <span>Ready to Trade, just type in your email, create a username and password</span>
 
                     <form>
-                        <input className="login-input" type="text" placeholder="First Name"></input>
-                        <input className="login-input" type="text" placeholder="Last Name"></input>
-                        <input className="login-input" type="text" placeholder="Email"></input>
-                        <input className="login-input" type="text" placeholder="Username"></input>
-                        <input className="login-input" type="password" placeholder="Password"></input>
+                        <input onChange={handleChange} name="firstname" className="login-input" type="text" placeholder="First Name"></input>
+                        <input onChange={handleChange} name="lastname" className="login-input" type="text" placeholder="Last Name"></input>
+                        <input onChange={handleChange} name="email" className="login-input" type="text" placeholder="Email"></input>
+                        <input onChange={handleChange} name="username" className="login-input" type="text" placeholder="Username"></input>
+                        <input onChange={handleChange} name="password" className="login-input" type="password" placeholder="Password"></input>
                     </form>
 
-                    <button>Register</button>
+                    <button onClick={handleSubmit}>Register</button>
+
+                    {err && err}
+
                     <span>Do you already have an account? Login here</span>
 
                     <Link to={"/login"}>
