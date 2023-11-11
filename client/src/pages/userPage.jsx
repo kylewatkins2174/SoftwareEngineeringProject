af35eeb1f60058141d72112544daf1a50989af24
Travis <travisiwillis515@gmail.com>
2023-11-11 13:18:00 -0600

    GeneralUpdate

--- client/src/pages/userPage.jsx
+++ client/src/pages/userPage.jsx
@@ -1,8 +1,32 @@
-import { Link } from 'react-router-dom';
 import './userPage.scss';
+import { useState } from "react"
+import { Link } from 'react-router-dom'
+import requestServer from "../axios"
 
 function User () {
-   
+
+    const [inputs, setInputs] = useState({
+        "email" : ""
+    })
+
+    const [err, setErr] = useState(null)
+
+    const handleChange= (e) => {
+        setInputs((prev) => {
+            return{...prev, [e.target.name] : e.target.value}
+        })
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(JSON.stringify(inputs))
+
+        requestServer.post("/auth/userPage", inputs).then(response => {
+            console.log(response)
+        }).catch(err => {
+            setErr(err)
+        })
+    }
     return(  
 
         <div className='bodyContainerUser'>
@@ -11,11 +35,11 @@ function User () {
             <div className='infoContainerUser'>
                 <h1 className='yourAccountUser'>Your Account</h1>
                 <form>
-                <input className="userNameUser" placeholder="Username"></input><br/>
-                <input className="emailUser" placeholder="email"></input><br/>
-                <input className="lastNameUser" type="text" placeholder="First Name"></input><br/>
-                <input className="firstNameUser" type="text" placeholder="Last Name"></input><br/><br/>
-                <button className="infoButtonUser" type="submit">Update</button>
+                <p className="userDataUser" placeholder="Username"></p><br/>
+                <p onChange={handleChange} className="emailUser" placeholder="email"></p><br/>
+                <p className="userDataUser" type="text" placeholder="First Name"></p><br/>
+                <input className="userDataUser" type="text" placeholder="Last Name"></input><br/><br/>
+                <button onChange={handleSubmit} className="infoButtonUser" type="submit">Update</button>
                 </form>
             </div>
         </div>
