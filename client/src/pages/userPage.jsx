0e527d5fa98ab1ebfc97b403bd5382ef76154f4b
Travis <travisiwillis515@gmail.com>
2023-11-14 17:11:11 -0600

    scriptandcssUpdate

--- client/src/pages/userPage.jsx
+++ client/src/pages/userPage.jsx
@@ -1,32 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './userPage.scss';
-import { useState } from "react"
-import { Link } from 'react-router-dom'
 import requestServer from "../axios"
 
 function User () {
-
-    const [inputs, setInputs] = useState({
-        "email" : ""
-    })
-
-    const [err, setErr] = useState(null)
-
-    const handleChange= (e) => {
-        setInputs((prev) => {
-            return{...prev, [e.target.name] : e.target.value}
+    
+        const [inputs, setInputs] = useState({
+            "firstname" : "",
+            "lastname" : "",
+            "email" : ""
         })
-    }
-
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        console.log(JSON.stringify(inputs))
-
-        requestServer.post("/auth/userPage", inputs).then(response => {
-            console.log(response)
-        }).catch(err => {
-            setErr(err)
-        })
-    }
+    
+        const [err, setErr] = useState(null)
+    
+        const handleChange = (e) => {
+            setInputs((prev) => {
+                return{...prev, [e.target.name] : e.target.value}
+            })
+        }
+    
+        const handleUpdate = (e) =>{
+            e.preventDefault();
+            console.log(JSON.stringify(inputs))
+    
+            requestServer.post("/auth/userPage", inputs).then(response => {
+                console.log(response)
+            }).catch(err => {
+                setErr(err)
+            })
+        }
+      /*  const [getThings, setThings] = useState([]);
+        useEffect(()=>{
+            const getThings= async ()=>{
+                const res = fetch('');
+                const getdata = (await res).json();
+                setThings(getdata);
+                console.log(getdata);
+            }
+            getThings();
+        },[]);*/
+   
     return(  
 
         <div className='bodyContainerUser'>
@@ -35,11 +48,11 @@ function User () {
             <div className='infoContainerUser'>
                 <h1 className='yourAccountUser'>Your Account</h1>
                 <form>
-                <p className="userDataUser" placeholder="Username"></p><br/>
-                <p onChange={handleChange} className="emailUser" placeholder="email"></p><br/>
-                <p className="userDataUser" type="text" placeholder="First Name"></p><br/>
-                <input className="userDataUser" type="text" placeholder="Last Name"></input><br/><br/>
-                <button onChange={handleSubmit} className="infoButtonUser" type="submit">Update</button>
+                <p className="userDataUser">Your Username</p><br/>
+                <input onChange={handleChange} /*name="email"*/className="inputUser" /*key={email.userId}*/ type="text" placeholder="First Name"/*email.name*/></input><br/>
+                <input onChange={handleChange} /*name="firstname"*/ className="inputUser" /*key={firstname.userId}*/ type="text" placeholder="First Name"/*firstname.name*/></input><br/>
+                <input onChange={handleChange} /*name="lastname"*/ className="inputUser" /*key={lastname.userId}*/ type="text" placeholder="Last Name"/*lastname.name*/></input><br/><br/>
+                <button onChange={handleUpdate} className="infoButtonUser" type="submit">Update</button>
                 </form>
             </div>
         </div>
