3a480ab721613e1537dd7b5250a699357bfbf72d
Kelly Castillo <kellycastillo123@gmail.com>
2023-11-14 18:57:18 -0600

    commented out home protect

--- client/src/App.js
+++ client/src/App.js
@@ -59,9 +59,9 @@ function App() {
     {
       path:"/home",
       element: (
-        <ProtectedRoute>
+       // <ProtectedRoute>
           <Homepage/>
-        </ProtectedRoute>
+       // </ProtectedRoute>
       )
     },
     {
